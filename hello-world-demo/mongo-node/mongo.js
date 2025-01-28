a7fafb24b43281a37987c18675e84569fb7c028b
vd.evan <evan.vanduzer@aol.com>
2025-01-28 11:44:39 -0500

    Added Regions to mongo.js
    https://marketplace.visualstudio.com/items?itemName=maptz.regionfolder
    
    https://marketplace.visualstudio.com/items?itemName=SantaCodes.santacodes-region-viewer

--- hello-world-demo/mongo-node/mongo.js
+++ hello-world-demo/mongo-node/mongo.js
@@ -1,23 +1,27 @@
-// Definitions / Requirements
+/* #region Definitions / Requirements */
 const EXPRESS = require('express');
 const APP = EXPRESS();
 const PORT = 8887;
 const BODY_PARSER = require('body-parser');
 const CORS = require('cors');
 const MONGOOSE = require('mongoose');
+/* #endregion */
 
-// Middleware
+/* #region Middleware */
 APP.use(BODY_PARSER.json());
 APP.use(CORS());
+/* #endregion */
 
-// Connect to MongoDB
+/* #region MongoDB Connection */
 const DATABASE_URL = 'mongodb://0.0.0.0:27017/sheridan';
 MONGOOSE.set('strictQuery', true);
 MONGOOSE.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
+/* #endregion */
 
-// Start the server
+/* #region Server Start */
 APP.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}!`);
 });
+/* #endregion */
