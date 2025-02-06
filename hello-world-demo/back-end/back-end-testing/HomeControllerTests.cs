4e8e165a1f64d43856e40feb17927540a409ea4d
Ahmed-Muqarrib <muqarrib@sheridancollege.ca>
2025-02-06 00:01:02 -0500

    Adding xUnit Testing to C#
    Added a testing environment with 1 sample test.

---
+++ hello-world-demo/back-end/back-end-testing/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace back_end.Test
+{
+    public class HomeControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public HomeControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Index_Get_ReturnsHelloMessage()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Ensure the request was successful
+            Assert.Contains("Welcome to the Courage Culture Club", content); // Check if the "hello" message is in the response
+        }
+    }
+}
