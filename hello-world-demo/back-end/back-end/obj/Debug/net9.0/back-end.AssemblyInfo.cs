62b57a4d725e68ff7e2512d2ff311aa72c0f45d3
Robert Orazu <orazurobert@yahoo.com>
2025-03-08 16:18:53 -0500

    Created Necessary folders

--- hello-world-demo/back-end/back-end/obj/Debug/net9.0/back-end.AssemblyInfo.cs
+++ hello-world-demo/back-end/back-end/obj/Debug/net9.0/back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea1f97b22987abbe962d597a3fe79314a402516")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6e6836a1069c65e2dc85ea0a75156a91fc91bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
